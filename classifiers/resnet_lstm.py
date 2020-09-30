8bf5c784ff6aacac8391660e49a5656b673fc420
Surayez Rahman <surayezrahman@Surayezs-MBP.hub>
2020-09-30 17:53:42 +1000

    fix

--- classifiers/resnet_lstm.py
+++ classifiers/resnet_lstm.py
@@ -158,7 +158,7 @@ class Classifier_ResNet_LSTM:
                                                       min_lr=0.0001)
 
         file_path = self.output_directory + 'best_model.h5'
-        model_checkpoint = keras.callbacks.ModelCheckpoint(filepath=file_path, monitor='val_accuracy', mode='max',
+        model_checkpoint = keras.callbacks.ModelCheckpoint(filepath=file_path, monitor='val_auc', mode='max',
                                                            save_best_only=True)
 
         self.callbacks = [reduce_lr, model_checkpoint]
