36496b14957a7eedb1a7e42cf3077628221a336a
Surayez Rahman <surayez.dev01@gmail.com>
2020-09-30 16:07:41 +1000

    fix

--- classifiers/attention_classifier.py
+++ classifiers/attention_classifier.py
@@ -80,12 +80,12 @@ class Classifier_Attention:
             # https://www.tensorflow.org/api_docs/python/tf/keras/callbacks/ModelCheckpoint
             reduce_lr = keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=50,
                                                           min_lr=0.0001)
-            model_checkpoint = keras.callbacks.ModelCheckpoint(filepath=file_path, monitor='val_acc', mode='max',
+            model_checkpoint = keras.callbacks.ModelCheckpoint(filepath=file_path, monitor='val_accuracy', mode='max',
                                                                save_best_only=True)
         else:
             reduce_lr = keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.5, patience=50,
                                                           min_lr=0.0001)
-            model_checkpoint = keras.callbacks.ModelCheckpoint(filepath=file_path, monitor='acc', mode='max', save_best_only=True)
+            model_checkpoint = keras.callbacks.ModelCheckpoint(filepath=file_path, monitor='accuracy', mode='max', save_best_only=True)
         self.callbacks = [reduce_lr, model_checkpoint]
 
         start_time = time.time()
